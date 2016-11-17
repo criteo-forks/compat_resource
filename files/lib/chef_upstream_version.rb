6b944733458bd031f21c43aad6f1de261a47ef91
Grégoire Seux <g.seux@criteo.com>
2016-11-17 09:18:49 +0100

    Avoid compat_resource patching for criteo chef versions
    Change-Id: Ida822ae9df7b95e29fb11132f50833abbf17cf9e

--- files/lib/chef_upstream_version.rb
+++ files/lib/chef_upstream_version.rb
@@ -1,3 +1,4 @@
         module ChefCompat
-          CHEF_UPSTREAM_VERSION="12.16.42"
+          # g.seux: We don't want any modification on chef classes from this cookbook
+          CHEF_UPSTREAM_VERSION="12.5.1" # used to be 12.16.x
         end
